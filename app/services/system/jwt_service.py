1fab76bfd5e91eb051c4a51bba8cb79be6a02ece
abcdzmxw <rulaiqiu.c@gmail.com>
2023-06-19 01:28:54 +0800

    x

--- app/services/system/jwt_service.py
+++ app/services/system/jwt_service.py
@@ -3,14 +3,14 @@ import jwt
 
 # 生成 JWT
 def generate_jwt(payload, secret_key):
-    token = jwt.encode(payload, secret_key, algorithm='HS256')
+    token = jwt.encode(payload=payload, key=secret_key, algorithm='HS256')
     return token
 
 
 # 解析 JWT
 def parse_jwt(token, secret_key):
     try:
-        payload = jwt.decode(token, secret_key, algorithms=['HS256'])
+        payload = jwt.decode(jwt=token, key=secret_key, algorithms=['HS256'])
         return payload
     except jwt.DecodeError:
         # JWT 解码错误
