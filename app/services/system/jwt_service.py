51b486a8d7eba259ec2d93faa5bd081e26c81a94
abcdzmxw <rulaiqiu.c@gmail.com>
2023-06-19 01:56:06 +0800

    x

--- app/services/system/jwt_service.py
+++ app/services/system/jwt_service.py
@@ -1,22 +1,9 @@
-# import jwt
+from app import utils
 
+logger = utils.get_logger()
 
 # 生成 JWT
 def generate_jwt(payload=None, secret_key=None):
     token = ""
-    # jwt.encode(payload=payload, key=secret_key, algorithm='HS256')
+    logger.info("进来了generate_jwt")
     return token
-
-
-# 解析 JWT
-def parse_jwt(token=None, secret_key=None):
-    # try:
-    payload = ""
-    # jwt.decode(jwt=token, key=secret_key, algorithms=['HS256'])
-    return payload
-    # except jwt.DecodeError:
-    #     # JWT 解码错误
-    #     return None
-    # except jwt.ExpiredSignatureError:
-    #     # JWT 过期错误
-    #     return None
