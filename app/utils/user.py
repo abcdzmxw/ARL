2c2b6bbfe9512b7cba44ea5d2e856536497a816b
abcdzmxw <rulaiqiu.c@gmail.com>
2023-06-19 01:29:45 +0800

    x

--- app/utils/user.py
+++ app/utils/user.py
@@ -1,9 +1,8 @@
-from flask import  request
-from app import modules
+from flask import request
+
 from app.config import Config
 from . import gen_md5, random_choices
 from .conn import conn_db
-from app.services.system.jwt_service import generate_jwt
 
 salt = 'arlsalt!@#'
 
