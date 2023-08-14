f80a098c77261cf9e2dbfaffab8433b573730db8
abcdzmxw <rulaiqiu.c@gmail.com>
2023-08-14 21:46:19 +0800

    x

--- docker/mongo-init.js
+++ docker/mongo-init.js
@@ -1,2 +1,2 @@
 db.user.drop()
-db.user.insert({ username: 'admin',  password: hex_md5('arlsalt!@#'+'arlpass') })
+db.user.insert({ username: 'admin',  password: hex_md5('at!3b@ia*rw#'+'arlpass') })
